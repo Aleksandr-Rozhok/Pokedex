e62293aa27165b9379ef2b9d819aa6b1fb57d04c
Razhok Aliaksandr <aliaksandr.razhok.std@gmail.com>
2024-08-30 18:11:25 +0400

    create a simple REPL app

--- main.go
+++ main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	type cliCommand struct {
+		name        string
+		description string
+		callback    func() error
+	}
+
+	commands := map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("pokedex > ")
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			continue
+		}
+
+		input = strings.TrimSpace(input)
+
+		if value, exists := commands[input]; exists {
+			err := value.callback()
+			if err != nil {
+				break
+			}
+		} else {
+			fmt.Println("Unknown command:", input)
+		}
+	}
+}
+
+func commandHelp() error {
+	fmt.Printf(`%vWelcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex
+%v`, "\n", "\n")
+
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
 }
